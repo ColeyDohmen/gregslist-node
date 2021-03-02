ce6f2e21e22d01ff4d13a8c2484407017eaea3ba
ColeyDohmen <coley.dawn12@gmail.com>
2021-03-02 16:42:59 -0700

    yearadded

--- server/models/House.js
+++ server/models/House.js
@@ -7,6 +7,7 @@ const House = new Schema(
     bathrooms: { type: Number, required: true },
     price: { type: Number, required: true },
     levels: { type: Number, required: true },
+    year: { type: Number, required: true },
     description: { type: String },
     imgUrl: {type: String, required: true, default: "http://placehold.it/200x200" }
   },
